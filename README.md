645bef7530946fdf88bd72e4a4ca7ed14ad835ed
unknown <LFairbrass@spartaglobal.com>
2022-10-13 12:32:55 +0100

    oop .py file added to showcase importing libraries

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Python Object-Oriented Programming
 ## The four pillars of OOP
-### Methods and Functions
-### Modules, Packages and Libraries
+#### Methods and Functions
+#### Modules, Packages and Libraries
+
+##### use case - benefits - examples of builtin modules - packages
+
+## Importing libraries
---
+++ oop_modules_packages.py
@@ -0,0 +1,29 @@
+# Lucky draw
+# keyword -> import
+
+# import random
+# Can also use:
+# from random import *
+# changes syntax for use though
+# import math
+
+
+# print(random) # Just shows where the imported file is
+# print(random()) # Gives a completely random number 0 - 1
+
+# Calculate DOB or year of birth
+
+from random import *
+import math
+
+number = 23.66
+# any numbers - to round figure up -
+# number 1.50 above round up = 2, 1.49 down round down = 1
+
+print(math.ceil(number)) # Rounds up to the nearest whole number - so even if number = 23.01 it will output 24
+print(math.floor(number)) # Round down to the nearest whole number - so even if number = 23.99 it will output 23
+
+
+
+
+
